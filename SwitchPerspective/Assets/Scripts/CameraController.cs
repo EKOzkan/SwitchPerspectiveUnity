aacf7bccded59fde9221bb65aca2f3d86322917c
EKOzkan <44098870+EKOzkan@users.noreply.github.com>
2022-12-11 21:05:11 +0300

    Added Project Files

---
+++ SwitchPerspective/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Animator animator;
+    bool isOrthographic = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCam();
+        }
+
+    }
+
+    void SwitchCam()
+    {
+        if (isOrthographic) {
+            animator.Play("PerspectiveCam");
+        }
+        else
+        {
+            animator.Play("FakeOrthographicCam");
+
+        }
+        isOrthographic = !isOrthographic;
+    }
+}
